6a08bcee7b707c64a224de09b7fb26ab22a1b1dc
telco2011 <davidlopez.david@gmail.com>
2019-11-30 20:18:00 +0100

    Code optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const {
-	resolve
-} = require('path');
+const { resolve } = require('path');
 
 module.exports = {
-	mode: process.env.NODE_ENV || 'development',
-	entry: {
-		main: './src/index.ts',
-	},
-	module: {
-		rules: [{
-			test: /\.tsx?$/,
-			use: {
-				loader: 'ts-loader',
-				options: {
-					configFile: "tsconfig.build.json"
-				}
-			}
-		}, ],
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
-	output: {
-		filename: 'volundr.js',
-		path: resolve('dist'),
-	},
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    main: './src/index.ts',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: 'tsconfig.build.json',
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'volundr.js',
+    path: resolve('dist'),
+  },
 };
