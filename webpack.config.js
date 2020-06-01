3a95517614807b81c8b562f1d352d99b479bc081
David <davidlopez.david@gmail.com>
2020-06-01 13:25:19 +0200

    chore: update build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const { resolve } = require('path');
-const TypedocWebpackPlugin = require('typedoc-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
@@ -26,13 +25,4 @@ module.exports = {
     filename: 'index.js',
     path: resolve('dist'),
   },
-  plugins: [
-    new TypedocWebpackPlugin({
-      name: 'Contoso',
-      mode: 'file',
-      theme: './typedoc-theme/',
-      includeDeclarations: false,
-      ignoreCompilerErrors: true,
-    }),
-  ],
 };
